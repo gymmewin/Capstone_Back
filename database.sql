f34202dd8ec947849e30af89c1cdb245727ad6d5
Jimmy Nguyen <gymmewin@Jimmys-MacBook-Pro.local>
2021-11-29 17:03:23 -0500

    updated ticket schema in database.sql

--- database.sql
+++ database.sql
@@ -4,7 +4,9 @@ CREATE TABLE tickets(
    tickets_id SERIAL PRIMARY KEY,
    item VARCHAR(50),
    description VARCHAR (255),
-   date timestamp
+   date timestamp,
+   completed boolean,
+   notes VARCHAR (255)
 );
 
 INSERT INTO tickets (item, description, date)
